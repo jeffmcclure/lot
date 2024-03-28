68db279f9069455f656bcdbaef89761bc8ec9d29
Jeff McClure <jeffmccluregithub@gmail.com>
2024-03-28 19:51:26 -0400

    docker.sh - unarchive supporting files

--- docker.sh
+++ docker.sh
@@ -75,7 +75,27 @@ function webserver {
 	docker rm nwn_nginx &>/dev/null
 
 	echo creating nwn_nginx
-	docker run -dit -p "${LOT_LOCAL_IP}:8000:80" --name nwn_nginx -v "$(pwd):/usr/share/nginx/html" nginx
+	#docker run -dit -p "${LOT_LOCAL_IP}:8000:80" --name nwn_nginx -v "$(pwd):/usr/share/nginx/html" abevoelker/docker-nginx: -c "${LOT_DIR}/nginx.conf"
+#	docker run -dit -p "${LOT_LOCAL_IP}:8000:80" --name nwn_nginx -v "$(pwd):/usr/share/nginx/html" -v "${LOT_DIR}/nginx.conf":/etc/nginx/nginx.conf:ro nginx
+
+  cat<<EOF >../default.conf
+server {
+    listen       80;
+    listen  [::]:80;
+    server_name  localhost;
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+        autoindex on;
+    }
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+       root   /usr/share/nginx/html;
+    }
+}
+EOF
+
+	docker run -dit -p "${LOT_LOCAL_IP}:8000:80" --name nwn_nginx -v "$(pwd):/usr/share/nginx/html" -v "${LOT_DIR}/default.conf":/etc/nginx/conf.d/default.conf:ro nginx
 }
 
 function nwn {
@@ -86,7 +106,7 @@ function nwn {
     NWN_PORT=5121
     NWN_MODULE=${LOT_MOD_NAME}
     NWN_PUBLICSERVER=0
-    NWN_SERVERNAME='The Lord of Terror ${LOT_VERSION}'
+    NWN_SERVERNAME="The Lord of Terror ${LOT_VERSION}"
     NWN_ONEPARTY=1
     NWN_DIFFICULTY=3
     NWN_AUTOSAVEINTERVAL=15
@@ -114,7 +134,7 @@ function nwsync {
 }
 
 if [ $# -eq 0 ]; then
-	set -- web nwn sync
+	set -- sync web nwn
 fi
 
 for i in "$@"; do
