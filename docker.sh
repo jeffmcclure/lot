ad612fcdad23e738c54a95baea0fd580fc956fbf
Jeff McClure <jeffmccluregithub@gmail.com>
2024-03-28 19:51:26 -0400

    docker.sh

--- docker.sh
+++ docker.sh
@@ -1,29 +1,33 @@
 #!/bin/zsh
 
-#cd $(dirname $0)
-cd /Users/jeffmcclure/lot
-cd webserver
+# Script has permissive and lazy error handling so it can be run the first time and subsequently without failing or reporting errors
 
-#docker run -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home -e NWN_ILR=0 -e NWN_PORT=5121 -e NWN_MODULE=lot_1_7_4 -e NWN_PUBLICSERVER=1 -e NWN_SERVERNAME='The Lord of Terror 1.7.4' -e NWN_NWSYNCURL='http://192.168.7.229:8000' urothis/nwserver:8193-buster
-#docker pull urothis/nwserver:8193-buster
-#docker run -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home -e NWN_ILR=0 -e NWN_PORT=5121 -e NWN_MODULE=lot_1_7_4 -e NWN_PUBLICSERVER=1 -e NWN_SERVERNAME='The Lord of Terror 1.7.4'  urothis/nwserver:8193-buster
-#docker run -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home -e NWN_ILR=0 -e NWN_PORT=5121 -e NWN_MODULE=lot_1_7_4 -e NWN_PUBLICSERVER=1 -e NWN_SERVERNAME='The Lord of Terror 1.7.4' beamdog/nwserver:8193.34.1-arm64
-#docker run -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home -e NWN_ILR=0 -e NWN_PORT=5121 -e NWN_MODULE=lot_1_7_4 -e NWN_PUBLICSERVER=1 -e NWN_SERVERNAME='The Lord of Terror 1.7.4' urothis/nwserver
-#docker run -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home -e NWN_ILR=0 -e NWN_PORT=5121 -e NWN_MODULE=lot_1_7_4 -e NWN_PUBLICSERVER=1 -e NWN_SERVERNAME='The Lord of Terror 1.7.4' urothis/nwserver
+# Input variables
+# LOT_DIR     directory to run docker out of
+
+export LOT_LOCAL_IP=192.168.7.229
+export LOT_PUBLIC_IP=71.254.0.211
+export LOT_DIR=/Users/jeffmcclure/lot
+export LOT_MOD_NAME=lot_1_7_4
+export LOT_VERSION="1.7.4"
+
+mkdir $LOT_DIR 2>/dev/null
+echo cd $LOT_DIR
+cd $LOT_DIR
+mkdir lot_docker webserver 2>/dev/null
+cd webserver
 
 function webserver {
 	cd ../webserver
 
 	echo stopping nwn_nginx
-	docker stop nwn_nginx 2>/dev/null >/dev/null
+	docker stop nwn_nginx &>/dev/null
 
 	echo removing nwn_nginx
-	docker rm nwn_nginx 2>/dev/null >/dev/null
+	docker rm nwn_nginx &>/dev/null
 
 	echo creating nwn_nginx
-	#docker run -dit -p 192.168.7.229:8000:80 --name nwn_nginx -v $(pwd):/usr/share/nginx/html nginx
-	#docker run -dit -p 192.168.40.138:8000:80 --name nwn_nginx -v $(pwd):/usr/share/nginx/html nginx
-	docker run -dit -p 192.168.7.229:8000:80 --name nwn_nginx -v $(pwd):/usr/share/nginx/html nginx
+	docker run -dit -p "${LOT_LOCAL_IP}:8000:80" --name nwn_nginx -v "$(pwd):/usr/share/nginx/html" nginx
 }
 
 function nwn {
@@ -31,27 +35,24 @@ function nwn {
 	cat <<EOF >env.txt
     NWN_ILR=0
     NWN_PORT=5121
-    NWN_MODULE=lot_1_7_4
+    NWN_MODULE=${LOT_MOD_NAME}
     NWN_PUBLICSERVER=0
-    NWN_SERVERNAME='The Lord of Terror 1.7.4'
+    NWN_SERVERNAME='The Lord of Terror ${LOT_VERSION}'
     NWN_ONEPARTY=1
     NWN_DIFFICULTY=3
     NWN_AUTOSAVEINTERVAL=15
-    NWN_NWSYNCURL=http://71.254.0.211:8000
+    NWN_NWSYNCURL=http://${LOT_PUBLIC_IP}:8000
 EOF
 
-	#NWN_NWSYNCURL=http://ec2-35-91-182-7.us-west-2.compute.amazonaws.com
-	#NWN_NWSYNCURL=http://72.95.96.159:8000
-	#NWN_NWSYNCURL=http://192.168.7.229:8000
-
 	echo stopping nwn_lot
-	docker stop nwn_lot 2>/dev/null >/dev/null
+	docker stop nwn_lot &>/dev/null
 
 	echo removing nwn_lot
-	docker rm nwn_lot 2>/dev/null >/dev/null
+	docker rm nwn_lot &>/dev/null
 
-	echo copy lot_1_7_4.mod
-	/bin/cp /Users/jeffmcclure/Documents/Neverwinter\ Nights/modules/lot_1_7_4.mod modules/lot_1_7_4.mod
+	echo copy ${LOT_MOD_NAME}.mod
+	pwd
+	/bin/cp /Users/jeffmcclure/Documents/Neverwinter\ Nights/modules/${LOT_MOD_NAME}.mod modules/
 
 	echo creating nwn_lot
 	docker run --platform linux/amd64 -dit -p 5121:5121/udp --name nwn_lot -v $(pwd):/nwn/home --env-file=env.txt nwnxee/unified:build8193.36.11
@@ -60,11 +61,11 @@ EOF
 function nwsync {
 	echo nwsync_write
 	cd ../lot_docker
-	/Users/jeffmcclure/d/nwn/nwsync/bin/nwsync_write --description="My Cool Module Server Data V1" /Users/jeffmcclure/lot/webserver modules/lot_1_7_4.mod
+	/Users/jeffmcclure/d/nwn/nwsync/bin/nwsync_write --description="The Lord of Terror Server Data" /Users/jeffmcclure/lot/webserver modules/${LOT_MOD_NAME}.mod
 }
 
 if [ $# -eq 0 ]; then
-	set -- webserver nwn nwsync lotcp
+	set -- webserver nwn nwsync
 fi
 
 for i in "$@"; do
@@ -84,7 +85,7 @@ for i in "$@"; do
 
 	lotcp)
 		echo rsync to AWS
-		rsync -aP -e "ssh -i $AWS_KEY" /Users/jeffmcclure/lot/webserver/ ec2-user@$AWS:/usr/share/nginx/html/
+		rsync -aP -e "ssh -i $AWS_KEY" "${LOT_DIR}/webserver/" "ec2-user@${AWS}:/usr/share/nginx/html/"
 		;;
 	esac
 done
